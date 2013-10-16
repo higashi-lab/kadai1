5f0c86f213f46408b26acf25f5b3e059d00aedf8
s-nakamr <s-nakamr@ist.osaka-u.ac.jp>
2013-10-16 14:13:03 +0900

    switch kill ni taiou sitayo

--- load_balacer.rb
+++ load_balacer.rb
@@ -1,31 +1,30 @@
-class PacketIO < Controller
-
+class Packet < Controller
 	def start
-		@fdbs = {}
-	end	
+		@fdbList = {}
+	end
+
+	def switch_ready dpid
+		@fdbList[dpid.to_hex] = {}
+	end
+
+	def switch_disconnected dpid
+		@fdbList.delete(dpid.to_hex)
+	end
 
 	def packet_in dpid,message
-		switch = @fdbs[dpid]
-		if switch == nil
-			@fdbs[dpid] = {}
+		@fdbList[dpid.to_hex][message.macsa] = message.in_port
+		port = @fdbList[dpid.to_hex][message.macda] ? @fdbList[dpid.to_hex][message.macda] : OFPP_FLOOD
+		if @fdbList[dpid.to_hex][message.macda]
+			send_flow_mod_add(
+				dpid,
+				:match => Match.new( :in_port => port,
+														 :dl_dst => message.macda),
+				:actions => Trema::SendOutPort.new(port) )
 		end
-
-		@fdbs[dpid][message.macsa] = message.in_port
-		port = @fdbs[dpid][message.macda]
-
-		if port 
-			send_flow_mod_add(dpid,
-			:match => Match.from(message),
-			:actions => SendOutPort.new(port))
-			send_packet_out(dpid, 
-  			:packet_in => message,
-			:actions => Trema::SendOutPort.new(port))
-		else
-			send_packet_out(dpid, 
-  			:packet_in => message,
-			:actions => Trema::SendOutPort.new(OFPP_FLOOD))
-		end
-
-	end
+		send_packet_out(
+			dpid,
+			:packet_in => message,
+			:actions => Trema::SendOutPort.new(port) )
+	end 
 
 end
